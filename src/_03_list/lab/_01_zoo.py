65f23e00cddde2a7aad6c44138ac1bb85c4a3a49
Alex <alexanderranchev2@gmail.com>
2024-10-01 23:51:15 +0300

    1

--- src/_03_list/lab/_01_zoo.py
+++ src/_03_list/lab/_01_zoo.py
@@ -1,2 +1,8 @@
-def greet(name):
-    return f"Hello, {name}!" if name else "Hello, !"
+def body_parts(a, b, c):
+    return [c, b, a]
+
+if __name__ == '__main__':
+    a = input()
+    b = input()
+    c = input()
+    print(body_parts(a, b, c))
--- test/_03_list/lab/test_01_zoo.py
+++ test/_03_list/lab/test_01_zoo.py
@@ -1,6 +1,6 @@
 import pytest
 from src._03_list.lab import _01_zoo
 
-def test_greet():
-        assert _01_zoo.greet("Alice") == "Hello, Alice!"
+def test_body_parts():
+        assert _01_zoo.body_parts('T', 'B', 'H') == ['H', 'B', 'T']
 
