73a2aaa38d838281a2b8b0e96116654634dd4bb8
Alexander Ranchev <126092597+Aranchev@users.noreply.github.com>
2024-11-10 22:24:22 +0200

    Update _05_sort_names.py

--- src/_05_list_adv/lab/_05_sort_names.py
+++ src/_05_list_adv/lab/_05_sort_names.py
@@ -7,3 +7,14 @@ if __name__ == '__main__':
     a = input()
     print(sp_l(a))
 
+'''
+Softuni:
+strings = input().split(" ")
+searched_palindrome = input()
+palindromes = []
+for word in strings:
+    if word == "".join(reversed(word)):
+        opalindromes.append(word)
+print(f"{palindromes}")
+print(f"Found palindrome{palindromes.count(searched_palindrome)} times")
+'''
