d266bf2c1422ccafa1d4231029e433dc43e3d2b8
Alexander Ranchev <126092597+Aranchev@users.noreply.github.com>
2024-11-13 23:57:57 +0200

    Update _04_palin.py

--- src/_05_list_adv/lab/_04_palin.py
+++ src/_05_list_adv/lab/_04_palin.py
@@ -22,6 +22,16 @@ if __name__ == '__main__':
     print(d)
     print(f'Found palindrome {e} times')
 
-
+'''
+Softuni:
+strings = input().split(" ")
+searched_palindrome = input()
+palindromes = []
+for word in strings:
+    if word == "".join(reversed(word)):
+         palindromes.append(word)
+print(f"{palindromes}")
+print(f"Found palindrome{palindromes.count(searched_palindrome)} times")
+'''
 
 
